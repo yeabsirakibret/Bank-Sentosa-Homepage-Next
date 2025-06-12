a13feb15922fb2f3c17c374175b56232cf0b8069
Yeabsira Kibret <62602453+yeabsirakibret@users.noreply.github.com>
2025-06-12 18:52:01 +0300

    refactor: reorganize layout and improve HomePage structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import {NextIntlClientProvider} from 'next-intl';
 import {getLocale} from 'next-intl/server';
- 
+import "./globals.css";
 export default async function RootLayout({
   children
 }: {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,14 @@ export default function HomePage() {
   return (
     <div>
       <LanguageSwitch />
-      <h1>{t('title')}</h1>
+  
+      <h1 className="text-3xl font-bold underline">
+        {t('title')}
+      </h1>
+
+      <div className="min-h-screen flex items-center justify-center bg-blue-100">
+        <h1 className="text-4xl font-bold text-blue-900">Hello Tailwind!</h1>
+      </div>
     </div>
   );
 }
